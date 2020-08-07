ae5cbb10ac229a803546205b0195a93ea4c4c71d
ngodooz <51786413+ngodooz@users.noreply.github.com>
2020-08-07 17:18:33 -0400

    Add files via upload

---
+++ DS project code.R
@@ -0,0 +1,44 @@
+#Project code:
+
+
+
+
+#Create state_data  
+
+
+
+
+
+#Create data_by_state
+
+
+
+
+
+#Code for Siyu's Analysis
+
+
+
+
+
+#Code for Ngodoo's Analysis
+
+
+
+
+
+#Code for Brooke's Analysis
+
+
+
+
+
+
+#Code for Josh's Analysis
+
+
+
+
+
+
+  
